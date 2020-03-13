580c41f771d7a01647a692c0581900d33c126658
caiocfsa <caiocfsa@hotmail.com>
2020-03-13 10:26:18 -0300

    first commit

--- mobile/src/store/createStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, compose, applyMiddleware } from 'redux';
-
-export default (reducers, middlewares) => {
-  const enhancer =
-    __DEV__ === 'development'
-      ? compose(applyMiddleware(...middlewares), console.tron.createEnhancer())
-      : compose(applyMiddleware(...middlewares));
-  return createStore(reducers, enhancer);
-};
